6b779c6cfb5fdda6dde02fde38e053bc49e8522e
Keshinro Tobi <96826865+tobiii-driod@users.noreply.github.com>
2022-06-04 21:32:44 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
+(function ($) {
+  "use strict";
 
+  new WOW().init();
 
+ 
 
+  
 
   // background image
   function dataBackgroundImage() {
@@ -27,128 +32,8 @@
     dots: true,
   });
 
-  //product column responsive
-  $(".product_column3").slick({
-    centerMode: true,
-    centerPadding: "0",
-    slidesToShow: 5,
-    arrows: true,
-    rows: 2,
-    prevArrow:
-      '<button class="prev_arrow"><i class="ion-chevron-left"></i></button>',
-    nextArrow:
-      '<button class="next_arrow"><i class="ion-chevron-right"></i></button>',
-    responsive: [
-      {
-        breakpoints: 400,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoints: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoints: 992,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-        },
-      },
-      {
-        breakpoints: 1200,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 4,
-        },
-      },
-    ],
-  });
-
-  //for tooltip
-  $('[data-toggle="tooltip"]').tooltip();
-
-  //tooltip active
-  $(".action_links ul li a, .quick_button a").tooltip({
-    animated: "fade",
-    placement: "top",
-    container: "body",
-  });
-
-  //product row activation responsive
-  $(".product_row1").slick({
-    centerMode: true,
-    centerPadding: "0",
-    slidesToShow: 5,
-    arrows: true,
-    prevArrow:
-      '<button class="prev_arrow"><i class="ion-chevron-left"></i></button>',
-    nextArrow:
-      '<button class="next_arrow"><i class="ion-chevron-right"></i></button>',
-    responsive: [
-      {
-        breakpoints: 400,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoints: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoints: 992,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-        },
-      },
-      {
-        breakpoints: 1200,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 4,
-        },
-      },
-    ],
-  });
-
-  // blog section
-  $(".blog_column3").owlCarousel({
-    autoplay: true,
-    loop: true,
-    nav: true,
-    autoplayTimeout: 5000,
-    items: 3,
-    dots: false,
-    margin: 30,
-    navText: [
-      '<i class="ion-chevron-left"></i>',
-      '<i class="ion-chevron-right"></i>',
-    ],
-    responsiveClass: true,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      768: {
-        items: 2,
-      },
-      992: {
-        items: 3,
-      },
-    },
-  });
-
+ 
+ 
   //navactive responsive
   $(".product_navactive").owlCarousel({
     autoplay: false,
