6bd4c04d2992713938247baa078649685811c949
Keshinro Tobi <96826865+tobiii-driod@users.noreply.github.com>
2022-06-04 21:16:26 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,6 @@
-(function ($) {
-  "use strict";
 
-  new WOW().init();
 
-  //navbar cart
-  $(".cart_link > a").on("click", function () {
-    $(".mini_cart").addClass("active");
-  });
 
-  $(".mini_cart_close > a").on("click", function () {
-    $(".mini_cart").removeClass("active");
-  });
-
-  //sticky navbar
-  $(window).on("scroll", function () {
-    var scroll = $(window).scrollTop();
-    if (scroll < 100) {
-      $(".sticky-header").removeClass("sticky");
-    } else {
-      $(".sticky-header").addClass("sticky");
-    }
-  });
 
   // background image
   function dataBackgroundImage() {
